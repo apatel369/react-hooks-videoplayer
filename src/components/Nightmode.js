cfef076e54b7d7ee6fc88404da859331e8a79332
apatel369 <33442948+apatel369@users.noreply.github.com>
2019-10-05 21:46:52 -0500

    style nightmode

--- src/components/Nightmode.js
+++ src/components/Nightmode.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import StyledNightmode from './styles/StyledNightmode';
 
-const NightMode = props => (
-    <div>
-        NightMode!!!
-    </div>   
+const NightMode = ({ nightModeCallback, nightmode }) => (
+    <StyledNightmode>
+    <label className="switch">
+        <input type="checkbox" checked={nightmode} onChange={nightModeCallback} />
+        <span className="slider round" />
+    </label>
+    </StyledNightmode>
 )
 
 export default NightMode;
