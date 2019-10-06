a650026460473435e5693cb98f8082275294ab30
apatel369 <33442948+apatel369@users.noreply.github.com>
2019-10-06 16:59:02 -0500

    add routing with HOC

--- src/components/containers/PlaylistItems.js
+++ src/components/containers/PlaylistItems.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PlaylistItem from '../PlaylistItem';
+import withLink from '../hoc/withLink';
 import StyledPlaylistitems from '../styles/StyledPlaylistitems';
 
+const PlaylistItemWithLink = withLink(PlaylistItem);
 
 const PlayListItems = ({ videos, active }) => (
     <StyledPlaylistitems>
          {videos.map(video => (
-            <PlaylistItem
+            <PlaylistItemWithLink
                 key={videos.id}
                 video={video}
                 active={video.id === active.id ? true : false}
