d756bf4e148c46a795293056dabe5d0701b8c8f2
apatel369 <33442948+apatel369@users.noreply.github.com>
2019-10-05 21:55:19 -0500

    style playlistHeader

--- src/components/PlaylistHeader.js
+++ src/components/PlaylistHeader.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import StyledPlaylistHeader from './styles/StyledPlaylistHeader';
+import StyledJourney from './styles/StyledJourney';
 
-const PlayListHeader = props => (
-    <div>
-        PlayListHeader!!!
-    </div>   
-)
+const PlayListHeader = ({ active, total }) => (
+    <StyledPlaylistHeader>
+        <p>{active.title}</p>
+        <StyledJourney>
+            {active.num} / {total}
+        </StyledJourney>
+    </StyledPlaylistHeader>   
+);
 
 export default PlayListHeader;
